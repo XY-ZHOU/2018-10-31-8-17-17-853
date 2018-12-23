547ca2dc4eafc56bf805cd226e69ba28576a2aa7
XY-ZHOU <731986251@qq.com>
2018-12-23 22:18:03 +0800

    finish practice nine

--- main/practice_9/class.js
+++ main/practice_9/class.js
@@ -1,1 +1,15 @@
-
+class Class {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return ('Class ' + this.number);
+  }
+  assignLeader(student) {
+    return (this.number === student.klass.number) ?(this.leader = student):console.log('It is not one of us.');
+  }
+  appendMember(student) {
+    student.klass = this;
+  }
+}
+export {Class};
--- main/practice_9/teacher.js
+++ main/practice_9/teacher.js
@@ -1,1 +1,11 @@
-
+import {Person} from "./person";
+class Teacher extends Person {
+  constructor(name, age, id, klass) {
+    super(name, age, id);
+    this.klass = klass;
+  }
+    introduce() {
+    return (this.klass != undefined) ?(super.introduce() + ' I am a Teacher. I teach Class ' + this.klass.number + '.'):(super.introduce() + ' I am a Teacher. I teach No Class.');
+  }
+}
+export {Teacher};
