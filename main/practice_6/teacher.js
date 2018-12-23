e285a01abba75170f333e873fb1818dec0adb65d
XY-ZHOU <731986251@qq.com>
2018-12-23 22:15:23 +0800

    finish practice six

--- main/practice_6/teacher.js
+++ main/practice_6/teacher.js
@@ -1,1 +1,11 @@
-
+import {Person} from "./person";
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return (this.klass != undefined) ?(super.introduce() + ' I am a Teacher. I teach Class ' + this.klass + '.'):(super.introduce() + ' I am a Teacher. I teach No Class.');
+  }
+}
+export {Teacher};
