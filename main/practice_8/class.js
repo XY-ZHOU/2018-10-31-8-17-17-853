4f1919c4d80f008361e34d672a6b96206699c5ba
XY-ZHOU <731986251@qq.com>
2018-12-23 22:16:48 +0800

    finish practice eight

--- main/practice_8/class.js
+++ main/practice_8/class.js
@@ -1,1 +1,12 @@
-
+class Class {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return ('Class ' + this.number);
+  }
+  assignLeader(student) {
+    return (this.number === student.klass.number) ? (this.leader = student) : '';
+  }
+}
+export {Class};
--- main/practice_8/teacher.js
+++ main/practice_8/teacher.js
@@ -1,1 +1,11 @@
-
+import {Person} from "./person";
+class Teacher extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+    introduce() {
+    return (this.klass != undefined) ?(super.introduce() + ' I am a Teacher. I teach Class ' + this.klass.number + '.'):(super.introduce() + ' I am a Teacher. I teach No Class.');
+  }
+}
+export {Teacher};
